78311af828e3517b55be787fc22b2daa0ff9abb0
RUTVIK <rutvikchauhan002@gmail.com>
2024-10-13 15:12:06 +0530

    all changes done

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:8080/api/v1/user`, // Set the base URL",
+  baseURL: `https://quiz-web-7h64.onrender.com`, // Set the base URL",
   withCredentials: true,  // To send cookies if needed (depending on your setup)
   headers: {
     'Content-Type': 'application/json'
