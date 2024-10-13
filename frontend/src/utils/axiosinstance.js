8e5996eda2a58694354654e634c34524aba05e63
RUTVIK <rutvikchauhan002@gmail.com>
2024-10-13 18:20:12 +0530

    url change

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  // baseURL: `https://quiz-web-2.onrender.com/api/v1/user`, // Set the base URL",
-  baseURL: `http://localhost:8080/api/v1/user`, // Set the base URL",
+  baseURL: `https://quiz-web-2.onrender.com/api/v1/user`, // Set the base URL",
   withCredentials: true,  // To send cookies if needed (depending on your setup)
   headers: {
     'Content-Type': 'application/json'
